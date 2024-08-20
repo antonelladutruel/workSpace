a90ced61d65fb7ce93fce3435d5562c1be549536
cabrerarovetta <162040722+cabrerarovetta@users.noreply.github.com>
2024-08-20 19:45:17 -0300

    actualización del login
    mensaje de error + redirección a index.html

--- js/login.js
+++ js/login.js
@@ -1,16 +1,24 @@
 document.addEventListener('DOMContentLoaded', function() {
 
-    const btnAccess = document.getElementById("btnAccess");
-    btnAccess.addEventListener('click', function(event) {
-        const username = getElementById('username').value.trim();
-        const password = getElementById('password').value.trim();
+    function showAlertError() {
+        document.getElementById("alert-danger").classList.add("show");
+    }
+    
+    document.getElementById("btnAccess").addEventListener('click', function(event) {
+        
+        event.preventDefault();
+
+        let username = document.getElementById('username').value.trim();
+        let password = document.getElementById('password').value.trim();
         
         if ([username,password].includes("")) {
-            alert("Debes completar los campos")
+            showAlertError();
+            console.log("campos requeridos incompletos")
         } else {
-            event.preventDefault();
             window.location.href = "index.html";
         }
-    })
+    });
 });
+
+
 
