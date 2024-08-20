4a789e9bd0a27521390fea28e694550b3dda92c1
cabrerarovetta <162040722+cabrerarovetta@users.noreply.github.com>
2024-08-20 18:50:43 -0300

    Update login.js

--- js/login.js
+++ js/login.js
@@ -1,8 +1,16 @@
 document.addEventListener('DOMContentLoaded', function() {
-    const btnAccess = document.getElementById("btnAccess");
 
+    const btnAccess = document.getElementById("btnAccess");
     btnAccess.addEventListener('click', function(event) {
-        event.preventDefault();
-        window.location.href = "index.html";
-    });
+        const username = getElementById('username').value.trim();
+        const password = getElementById('password').value.trim();
+        
+        if ([username,password].includes("")) {
+            alert("Debes completar los campos")
+        } else {
+            event.preventDefault();
+            window.location.href = "index.html";
+        }
+    })
 });
+
