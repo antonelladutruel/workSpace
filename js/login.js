d9781c855cdc607fffe07264afd4b4a781c9cba2
antonelladutruel <antonelladutruel@gmail.com>
2024-08-21 18:45:24 -0300

    Pauta 4 y LocalStorage

--- js/login.js
+++ js/login.js
@@ -1,16 +1,16 @@
 document.addEventListener('DOMContentLoaded', function() {
-
     const btnAccess = document.getElementById("btnAccess");
+    
     btnAccess.addEventListener('click', function(event) {
-        const username = getElementById('username').value.trim();
-        const password = getElementById('password').value.trim();
-        
-        if ([username,password].includes("")) {
-            alert("Debes completar los campos")
-        } else {
-            event.preventDefault();
-            window.location.href = "index.html";
-        }
-    })
+        event.preventDefault();
+    const username = document.getElementById('username').value;
+    const password = document.getElementById('password').value;
+        if (username && password){
+        localStorage.setItem("nombreUsuario", username);
+    localStorage.setItem("contraseña", password);
+    window.location.href = "index.html"}
+    else {
+        alert("Debes completar los campos")
+    }
+    });
 });
-
