55f887f85a2f0dfb514b651fc45652de9d2891b2
antonelladutruel <antonelladutruel@gmail.com>
2024-08-28 19:33:55 -0300

    Responsive

--- js/products.js
+++ js/products.js
@@ -2,22 +2,30 @@ const DATA_URL = 'https://japceibal.github.io/emercado-api/cats_products/101.jso
 
 const container = document.getElementById("containerProduct");
 
-function showData(dataArray) {
+  function showData(dataArray) {
+    const container = document.getElementById('containerProduct'); // Asegúrate de tener un contenedor con este ID
+    container.innerHTML = ''; // Limpiar el contenido previo
+  
     for (const item of dataArray) {
       container.innerHTML += `
-      <img class="imgProduct"src=${item.image} alt=${item.name}>
-      <p class="dataProduct">
-      Nombre: ${item.name}
-      <br>
-      Descripción:${item.description}
-      <br>
-      Precio:${item.cost}
-      <br>
-      Cantidad vendidos:${item.soldCount}
-      </p>
+        <div class="col-md-6 mb-4">
+          <div class="product">
+            <img class="imgProduct img-fluid" src="${item.image}" alt="${item.name}">
+            <p class="dataProduct mt-2">
+              <strong>Nombre:</strong> ${item.name}
+              <br>
+              <strong>Descripción:</strong> ${item.description}
+              <br>
+              <strong>Precio:</strong> ${item.cost}
+              <br>
+              <strong>Cantidad vendidos:</strong> ${item.soldCount}
+            </p>
+          </div>
+        </div>
       `;
     }
   }
+  
 
 fetch(DATA_URL)
 .then(res => res.json())
