a15a73ad4e200de84ffa083afc211876720ac1a8
cabrerarovetta <162040722+cabrerarovetta@users.noreply.github.com>
2024-08-29 20:48:20 -0300

    responsive design
    @media para las 3 resoluciones

--- js/products.js
+++ js/products.js
@@ -2,41 +2,40 @@ const DATA_URL = 'https://japceibal.github.io/emercado-api/cats_products/101.jso
 
 const container = document.getElementById("containerProduct");
 
-  function showData(dataArray) {
-    const container = document.getElementById('containerProduct'); // Asegúrate de tener un contenedor con este ID
-    container.innerHTML = ''; // Limpiar el contenido previo
-  
-    for (const item of dataArray) {
-      container.innerHTML += `
+function showData(dataArray) {
+  const container = document.getElementById('containerProduct');
+  container.innerHTML = '';
+
+  for (const item of dataArray) {
+    container.innerHTML += `
         <div class="col-md-6 mb-4">
           <div class="imgProductCnt">
               <img class="imgProduct" src=${item.image} alt=${item.name}>
-        </div>
+          </div>
         
-        <div class="productName" class="genericProductInfo">
+          <div class="productName" class="genericProductInfo">
           ${item.name}
-        </div>
-        <div class"productData">
-        <div class="productDescription" class="genericProductInfo">
-          ${item.description}
-        </div>
+          </div>
 
+          <div class"productData">
+            <div class="productDescription" class="genericProductInfo">
+              ${item.description}
+            </div>
 
-        <div class="productCost" class="genericProductInfo">
-          <p> US$${item.cost} </p>
-        </div>
+            <div class="productCost" class="genericProductInfo">
+             <p><span class="price">US$ </span>${item.cost} </p>
+            </div>
 
-
-        <div class="productSoldCount" class="genericProductInfo">
-         <p>Cantidad de vendidos ${item.soldCount} </p> 
-        </div>
-        </div>
-      </div>`;
-    }
+            <div class="productSoldCount" class="genericProductInfo">
+              <p><span class="productSoldCountItem">Cantidad de vendidos: </span>${item.soldCount} </p> 
+            </div>
+          </div>
+        </div>`;
   }
-  
+}
+
 
 fetch(DATA_URL)
-.then(res => res.json())
-.then(data => showData(data.products))
-.catch(error => console.error())
+  .then(res => res.json())
+  .then(data => showData(data.products))
+  .catch(error => console.error())
