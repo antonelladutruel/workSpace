14aaa1ebf2b47fa0a09a3737ce1378b16b56967f
cabrerarovetta <162040722+cabrerarovetta@users.noreply.github.com>
2024-09-15 19:08:53 -0300

    info del producto ok

--- js/product-info.js
+++ js/product-info.js
@@ -1,23 +1,4 @@
-//1. que encuentre el json
-//2. que encuentre el product = id
-//3. cargar para cada etiqueta html la info que trae el objeto
-
-/*<div class="container" id="containercircle"></div>
-      <ul class="productList">
-        <li class="product-item">
-          <img src="img/" alt="ImgProducto 1">
-          <div id="containertitle">Producto:</div>
-          <div class="description">Descripción:</div>
-          <div class="price">$:</div>
-          <div class="productssold">Cantidad de vendidos: </div>
-          <input> <button id="containerbutton">
-            <p class="text">add to cart</p>
-          </button> </input>
-
-        </li>
-      </ul>
-    </div>*/
-    document.addEventListener('DOMContentLoaded', function() {
+document.addEventListener('DOMContentLoaded', function() {
     const catId = localStorage.getItem('catID');
     const DATA_URL = `https://japceibal.github.io/emercado-api/cats_products/${catId}.json`;
     const productId = localStorage.getItem('productId');
@@ -29,12 +10,12 @@
         const product = productsArray.find(p => p.id === parseInt(productId));
 
         if (product) {
-            // Mostrar la información del producto
-            document.getElementsByClassName("productName").textContent = product.name;
-            document.getElementsByClassName("productDescription").textContent = product.description;
-            document.getElementsByClassName("productCost").textContent = `${product.currency} ${product.cost}`;
-            document.getElementsByClassName("productSoldCount").textContent = `Cantidad de vendidos: ${product.soldCount}`;
-            document.getElementsByClassName("productImage").src = product.image;
+            // Mostrar la información del producto en las etiquetas HTML correspondientes
+            document.querySelector(".productName").textContent = product.name;
+            document.querySelector(".productDescription").textContent = product.description;
+            document.querySelector(".productCost").textContent = `${product.currency} ${product.cost}`;
+            document.querySelector(".productSoldCount").textContent = `Cantidad de vendidos: ${product.soldCount}`;
+            document.querySelector(".productImage").src = product.image;
         }
     })
     .catch(error => console.error('Error al cargar los datos del producto:', error));
