111a54266136465ea42344d289d00a06a52462bd
antonelladutruel <antonelladutruel@gmail.com>
2024-10-28 21:42:36 -0300

    CartProduct and Styles

--- js/cart.js
+++ js/cart.js
@@ -6,3 +6,25 @@ if (!productCart) {
 };
 
 
+const catId = localStorage.getItem('catID');
+    const productId = localStorage.getItem('productId');
+    const DATA_URL = `https://japceibal.github.io/emercado-api/cats_products/${catId}.json`;
+
+    // 1. Cargar información del producto
+    fetch(DATA_URL)
+    .then(res => res.json())
+    .then(data => {
+        const productsArray = data.products;
+        const product = productsArray.find(p => p.id === parseInt(productId));
+
+        if (product) {
+            document.querySelector(".productName").textContent = product.name;
+            document.querySelector(".productDescription").textContent = product.description;
+            document.querySelector(".productCost").textContent = `${product.currency} ${product.cost}`;
+            document.querySelector(".productImage").src = product.image;
+            
+        }
+    })
+    .catch(error => console.error('Error al cargar los datos del producto:', error));
+
+
